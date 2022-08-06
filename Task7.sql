cf215b7b21949e1f7cb96191e607be4af7dffb7f
Ahmet Cakmak <95977791+4hmetcakmak@users.noreply.github.com>
2022-08-06 09:41:39 +0300

    Update and rename Task7 to Task7.sql

--- Task7.sql
+++ Task7.sql
@@ -1,4 +1,4 @@
-Merhabalar,
+/*Merhabalar,
 
 Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
 
@@ -6,10 +6,10 @@ Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden ger�
 2. film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
 3. customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir? 4. city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
 
-Kolay Gelsin.
+Kolay Gelsin.*/
 
-CEVAPLAR:
-1.SELECT rating FROM film GROUP BY rating;
-2.SELECT replacement_cost, COUNT(*) FROM film GROUP BY replacement_cost HAVING COUNT(*) > 50;
-3.SELECT store_id,COUNT(*) FROM customer GROUP BY store_id;
-4.SELECT country_id, COUNT(*) FROM city GROUP BY country_id ORDER BY COUNT(*) DESC LIMIT 1;
+--CEVAPLAR:
+SELECT rating FROM film GROUP BY rating;
+SELECT replacement_cost, COUNT(*) FROM film GROUP BY replacement_cost HAVING COUNT(*) > 50;
+SELECT store_id,COUNT(*) FROM customer GROUP BY store_id;
+SELECT country_id, COUNT(*) FROM city GROUP BY country_id ORDER BY COUNT(*) DESC LIMIT 1;
